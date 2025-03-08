5c6700bd623ac9247b8e1feb9e6247d22f3d6062
Bruno H. Homem <brunohhomem@gmail.com>
2025-03-08 16:10:27 -0300

    feat: add logo to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,17 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Header() {
   return (
     <header className="">
+      <Image
+        className="fixed top-0 m-6 ml-10"
+        src="/logo.png"
+        alt="logo"
+        width={100}
+        height={100}
+        quality="80"
+      />
       <div
         className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full
       dark:bg-blue-950 dark:border-black/40"
